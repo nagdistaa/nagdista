58058ed09a6dbe0f1d295915714fd9eedea717ea
nagdista <nagdista@gmail.com>
2025-03-16 20:46:12 +0200

    add some project

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,8 +7,8 @@ projectSrc: string;
 
 const Card = ({projectName , projectSrc}:Iprops)=>{
 return(
-    <div className="flex items-center justify-between p-[15px] m-[5px] shadow  rounded-[8px] w-[100%] md:w-[48%] lg:w-[30%]">
-        <h1 className="capitalize text-[18px]">{projectName}</h1>
+    <div className="flex items-center justify-between p-[15px] m-[5px] shadow  rounded-[8px] w-[100%] md:w-[48%] lg:w-[30%] border-[2px] border-gray-200">
+        <h1 className="capitalize text-[18px] ">{projectName}</h1>
        <div className="bg-red-600 text-white p-[3px] rounded-full flex items-center justify-center">
        <a href={projectSrc}><Eye  /></a>
        </div>
