7e70d3d316147d4d52b71dbe6c5ac02227c0584d
nagdista <nagdista@gmail.com>
2025-03-16 21:18:31 +0200

    enhance navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,9 +2,9 @@ import { Github } from "lucide-react";
 
 const NavBar = ()=>{
 return(
-    <div className="bg-white flex justify-between items-center shadow sticky z-100 top-0 left-0 w-full p-[15px] mb-[20px]">
-        <h1 className="text-black font-[700] text[22px] cursor-pointer">N<span className="text-red-500">agdista</span></h1>
-        <a href="https://github.com/nagdistaa"><Github  /></a>
+    <div className="bg-red-600 flex justify-between items-center shadow sticky z-100 top-0 left-0 w-full p-[15px] mb-[20px]">
+        <h1 className="text-white font-[700]  text-[25px] cursor-pointer">Nagdista</h1>
+        <a href="https://github.com/nagdistaa" className=""><Github className="text-white" size={30}/></a>
     </div>
 )
 }
