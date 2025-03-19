180170fc18a6d8f1ef4a71d7b54fe2ee84cbb8aa
nagdista <nagdista@gmail.com>
2025-03-19 11:55:15 +0200

    fix data

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Card from './components/Card'
 import Footer from './components/Footer'
 import NavBar from './components/NavBar'
+import {projectsData} from "./data/projects"
 
 function App() {
 
@@ -10,21 +11,12 @@ function App() {
     <div className='h-[100vh] relative'>
     <NavBar/>
    <div className="cards w-full flex justify-between items-center flex-wrap px-[15px]">
-    {/* loader  */}
-   <Card projectName='landing loader' projectSrc='https://nagdista-loader.netlify.app/'/>
-   {/* store */}
-   <Card projectName=' store' projectSrc='https://nagdista-store.netlify.app/'/>
-   {/* dictionary */}
-   <Card projectName='dictionary' projectSrc='https://nagdista-dictionary.netlify.app/'/>
-   {/* youtube audio */}
+    
 
-   <Card projectName='youtube audio' projectSrc='https:// nagdista-youtube-audio.netlify.app'></Card>
-  {/*  */}
-   <Card projectName='radio' projectSrc='https://nagdista-radio.netlify.app/'></Card>
-  {/*  */}
-  
-   <Card projectName='battery' projectSrc='https://nagdista-battarey.netlify.app/'></Card>
-  {/*  */}
+
+    {projectsData.map((el , idx)=>{
+      return(   <Card projectName= {el.projectName} projectSrc= {el.projectSrc}/>)
+    })}
   
    </div>
    <Footer author='Mhmoud Alnagdy' />
---
+++ src/data/projects.ts
@@ -0,0 +1,22 @@
+export const projectsData = [{
+    projectName :"landing loader",
+    projectSrc :"https://nagdista-loader.netlify.app/"
+},{
+    projectName :"store | NTI project",
+    projectSrc :"https://nagdista-store.netlify.app/"
+},{
+    projectName :"dictionary",
+    projectSrc :"https://nagdista-dictionary.netlify.app/"
+},{
+    projectName :"youtube audio",
+    projectSrc :"https:// nagdista-youtube-audio.netlify.app"
+},{
+    projectName :"radio",
+    projectSrc :"https://nagdista-radio.netlify.app/"
+},{
+    projectName :"battery",
+    projectSrc :"https://nagdista-battarey.netlify.app/"
+},{
+    projectName :"beautiful toggle button",
+    projectSrc :"nagdista-menu.netlify.app"
+},]
